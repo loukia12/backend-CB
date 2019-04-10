0243f68ab8c6ee1246b623014850ece3fe3c111f
Chara Syrtari <charas@mkinet.net>
2019-04-10 12:58:33 +0300

    new filters

--- src/main/java/com/example/vikings/demo/HouseController.java
+++ src/main/java/com/example/vikings/demo/HouseController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,14 +18,30 @@ public class HouseController {
     @GetMapping("/house")
     public List<House> index(){
     	System.out.println("YES");
-//    	return houseRepository.findAll();
-        return houseRepository.findByPrice(300);
+    	return houseRepository.findAll();        
     }
 	
-    
     @GetMapping("/house/{id}")
     public House show(@PathVariable String id){
         int houseId = Integer.parseInt(id);
         return houseRepository.getOne(houseId);
+    }
+    
+    @PostMapping("/house/price")
+    public List<House> showByPrice(@RequestParam(value = "toPrice") String toPrice){
+        int p = Integer.parseInt(toPrice);
+        return houseRepository.findByPrice(p);
+    }
+    
+    @GetMapping("/house/lowerSize")
+    public List<House> showByLowerSize(@RequestParam(value = "toSize") String toSize){
+        int s = Integer.parseInt(toSize);
+        return houseRepository.findBySizeLower(s);
+    }
+    
+    @GetMapping("/house/higherSize")
+    public List<House> showByHigherSize(@RequestParam(value = "fromSize") String fromSize){
+        int s = Integer.parseInt(fromSize);
+        return houseRepository.findBySizeHigher(s);
     }
 }
--- src/main/java/com/example/vikings/demo/HouseRepository.java
+++ src/main/java/com/example/vikings/demo/HouseRepository.java
@@ -3,13 +3,18 @@ package com.example.vikings.demo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HouseRepository extends JpaRepository<House, Integer> {
 
-	List<House> findById(int id);
-	List<House> findByPrice(int price);
-	List<House> findBySize(int size);
+	@Query("SELECT h FROM House h WHERE h.price = ?1 or h.price < ?1")
+    List<House> findByPrice(Integer price);
 	
+	@Query("SELECT h FROM House h WHERE h.size = ?1 or h.size < ?1")
+	List<House> findBySizeLower(Integer size);
+	
+	@Query("SELECT h FROM House h WHERE h.size = ?1 or h.size > ?1")
+	List<House> findBySizeHigher(Integer size);
 }
