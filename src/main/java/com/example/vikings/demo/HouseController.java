3fe01f2414908d310ae2c2a31689cf572d66403e
billgero <gerokostas@outlook.com.gr>
2019-04-11 15:52:38 +0300

    CrossOrigin

--- src/main/java/com/example/vikings/demo/HouseController.java
+++ src/main/java/com/example/vikings/demo/HouseController.java
@@ -3,12 +3,14 @@ package com.example.vikings.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class HouseController {
 
