f257925dd39781d91b4ba793cb9c0f5fbd1df517
Chara Syrtari <48282065+charasyr@users.noreply.github.com>
2019-04-17 14:21:10 +0300

    Update HouseController.java
    new controller

--- src/main/java/com/example/vikings/demo/HouseController.java
+++ src/main/java/com/example/vikings/demo/HouseController.java
@@ -14,45 +14,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HouseController {
 
+
 	@Autowired
 	HouseRepository houseRepository;
 	
+//	@RequestMapping(value = "/test")
+//	public String index2() {
+//		return "index";
+//	}
+	
     @GetMapping("/house")
     public List<House> index(){
-    	System.out.println("YES");
     	return houseRepository.findAll();        
     }
-	
-    @GetMapping("/house/{id}")
-    public House show(@PathVariable String id){
-        int houseId = Integer.parseInt(id);
-        return houseRepository.getOne(houseId);
-    }
     
-    @PostMapping("/house/price")
-    public List<House> showByPrice(@RequestParam(value = "toPrice") String toPrice){
-        int p = Integer.parseInt(toPrice);
-        return houseRepository.findByPrice(p);
-    }
-    
-    @GetMapping("/house/lowerSize")
-    public List<House> showByLowerSize(@RequestParam(value = "toSize") String toSize){
-        int s = Integer.parseInt(toSize);
-        return houseRepository.findBySizeLower(s);
-    }
-    
-    @GetMapping("/house/higherSize")
-    public List<House> showByHigherSize(@RequestParam(value = "fromSize") String fromSize){
-        int s = Integer.parseInt(fromSize);
-        return houseRepository.findBySizeHigher(s);
-    }
-    
-    @GetMapping("/house/results")
-    public List<House> showResults(@RequestParam(value = "toPrice", defaultValue = "1000000") String toPrice,
-    		@RequestParam(value = "toSize", defaultValue = "1000") String toSize){
+    //@CrossOrigin("http://localhost:3000/results")
+    @GetMapping("/results")
+    public List<House> showNewResults(@RequestParam(value = "toPrice", defaultValue = "1000") String toPrice,
+    		@RequestParam(value = "fromSize", defaultValue = "0") String fromSize, @RequestParam(value = "location") String location, 
+    		@RequestParam(value = "status", defaultValue = "Rent") String status){
     	
     	int p = Integer.parseInt(toPrice);
-    	int s = Integer.parseInt(toSize);
-        return houseRepository.findByFilters(p, s);
+    	int s = Integer.parseInt(fromSize);
+    	
+    	if(location.equals("")) {
+    		return houseRepository.findByFilters(p, s, status);
+    	}else {
+    		return houseRepository.findByFilters2(p, s, location, status);
+    	}
+
     }
 }
