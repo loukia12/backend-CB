ce58e446cc2ea69ca3f20fa4a1c0713b3cc59b68
Chara Syrtari <48282065+charasyr@users.noreply.github.com>
2019-05-30 15:09:01 +0300

    image param in addHouse method

--- src/main/java/com/example/vikings/demo/HouseController.java
+++ src/main/java/com/example/vikings/demo/HouseController.java
@@ -1,26 +1,17 @@
 package com.example.vikings.demo;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
-//@CrossOrigin(maxAge = 3600)
 @RestController
 public class HouseController {
-
 
 	@Autowired
 	HouseRepository houseRepository;
@@ -35,7 +26,7 @@ public class HouseController {
     @GetMapping("/house/results")
     public List<House> showNewResults(@RequestParam(value = "toPrice", defaultValue = "100000") String toPrice,
     		@RequestParam(value = "fromSize", defaultValue = "0") String fromSize, @RequestParam(value = "location", defaultValue = "") String location, 
-    		@RequestParam(value = "status", defaultValue = "Buy") String status){
+    		@RequestParam(value = "status", defaultValue = "Rent") String status){
     	
     	int p = Integer.parseInt(toPrice);
     	int s = Integer.parseInt(fromSize);
@@ -53,37 +44,20 @@ public class HouseController {
     		return houseRepository.findByFilters2(p, s, location, status);
     	}
     }
-    
+      
     @CrossOrigin("http://localhost:3000/yourEntry")
-    @PostMapping(path= "/add")
-    @ResponseBody
-    public  String addHouse(@RequestParam Map<String, String> body)
-        {
-    	String price=body.get("price");
+    @GetMapping("/add")
+    public String addHouse(@RequestParam(value = "price") String price, @RequestParam(value = "size") String size, 
+    		@RequestParam(value = "location") String location, @RequestParam(value = "status") String status, @RequestParam(value = "contact") String contact, 
+    		@RequestParam(value = "image") String image){
     	
-    	
-    	String size=body.get("size");
-    	
-    	String contact = body.get("contact");
-    	
-    	
-    	String location=body.get("location");
-    	System.out.println(location);
-    	
-    	String status=body.get("status");
-    	System.out.println(status);
-	 
     	int p = Integer.parseInt(price);
-    	System.out.println(p);
-    	
     	int s = Integer.parseInt(size);
-    	System.out.println(s);
     	
-    	House h= new House(p,s,status,contact,location);
+    	House h= new House(p, s, status, contact, location, image);
     	houseRepository.save(h);
     	
-    	
     	System.out.print("Added");
-    	return ("New house added");	
+    	return ("New house added");
     }
 }
